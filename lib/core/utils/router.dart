3141e3f769eab1d4cc99e518a2c58faa383b9af4
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-11 07:34:14 +0300

    create onBoarding screen and navigate to it

--- lib/core/utils/router.dart
+++ lib/core/utils/router.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:news_app2/features/home/presentation/home_screen.dart';
+import 'package:news_app2/features/onBoarding/presentation/on_boarding_screen.dart';
 import 'package:news_app2/features/onBoarding/presentation/splash_screen.dart';
 
 class AppRouter {
 
   static const kHomeScreen = '/homeScreen';
+  static const kOnBoardingScreen = '/onBoardingScreen';
 
   static Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -12,6 +14,8 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case kHomeScreen:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case kOnBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
     }
     return null;
   }
