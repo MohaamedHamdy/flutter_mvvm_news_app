7f7212ec46ece71ed58447cebc1277af5e23b882
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-11 07:23:31 +0300

    implement styles file for now (change later)

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+class AppStyle {
+  static TextStyle style10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle style12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle style14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle style16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle style18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle style20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle style22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle style24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+  );
+}
