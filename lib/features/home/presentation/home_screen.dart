0458e3dbd5d579afe6439db9fc2986a151a74e2d
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-11 07:31:34 +0300

    navigate from splash to home after 2 seconds

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/home_screen_body.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: HomeScreenBody(),
+    );
   }
 }
--- lib/features/onBoarding/presentation/splash_screen.dart
+++ lib/features/onBoarding/presentation/splash_screen.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:news_app2/core/utils/constants.dart';
+import 'package:news_app2/core/utils/router.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2),
+        () => Navigator.pushNamed(context, AppRouter.kHomeScreen));
+  }
 
   @override
   Widget build(BuildContext context) {
