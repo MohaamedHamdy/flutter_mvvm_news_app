21c21503a95f057a50bbc43a42892fca8442b7c1
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-11 07:26:42 +0300

    create home screen

--- lib/core/utils/router.dart
+++ lib/core/utils/router.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:news_app2/features/home/presentation/home_screen.dart';
 import 'package:news_app2/features/onBoarding/presentation/splash_screen.dart';
 
 class AppRouter {
+
+  static const kHomeScreen = '/homeScreen';
+
   static Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case kHomeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
     }
     return null;
   }
