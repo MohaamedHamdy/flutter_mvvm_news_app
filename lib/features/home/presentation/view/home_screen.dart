13deabd50deefcffc6a07b95a69eebd907c3dd62
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-17 12:17:09 +0300

    home bottom navigation bar added

--- lib/features/home/presentation/view/home_screen.dart
+++ lib/features/home/presentation/view/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:news_app2/core/utils/constants.dart';
-
 import 'widgets/home_screen_body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +7,25 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeScreenBody(),
+    return Scaffold(
+      body: const HomeScreenBody(),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          currentIndex: 0,
+          selectedItemColor: kPrimaryColor,
+          unselectedItemColor: Colors.white.withOpacity(0.5),
+          elevation: 0.0,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.explore_outlined), label: 'Explore'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark_border), label: 'Bookmark'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_outlined), label: 'Profile'),
+          ]),
     );
   }
 }
